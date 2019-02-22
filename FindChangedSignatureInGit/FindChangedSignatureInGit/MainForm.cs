a828cd73b73f8f9252e4b83bdc1a2a1296112980
niloofarf <fathi.niloofar@gmail.com>
2019-02-22 20:45:30 +0330

    Find all modified signature

--- FindChangedSignatureInGit/FindChangedSignatureInGit/MainForm.cs
+++ FindChangedSignatureInGit/FindChangedSignatureInGit/MainForm.cs
@@ -17,6 +17,8 @@ namespace FindChangedSignatureInGit
 {
     public partial class MainForm : Form
     {
+        private int gridRowCounter = 0;
+
         public MainForm()
         {
             InitializeComponent();
@@ -26,6 +28,14 @@ namespace FindChangedSignatureInGit
         {
 
             string repoAddress = "";
+            int commitCounter = 0;
+            int fileCounter = 0;
+            List<string> plusList = new List<string>();
+            List<string> minusList = new List<string>();
+            string fileName = "";
+            Regex javaFunctionsRegex = new Regex(@"^\+[ \t]*[a-zA-Z]*[ \t]*[a-zA-Z_][a-zA_Z0-9_]+[ \t]*\([a-zA-Z0-9_, ]*\)$");
+            Regex javaFunctionsRegex2 = new Regex(@"^-[ \t]*[a-zA-Z]*[ \t]*[a-zA-Z_][a-zA_Z0-9_]+[ \t]*\([a-zA-Z0-9_, ]*\)$");
+
 
             FolderBrowserDialog directorySelectionDialog = new FolderBrowserDialog();
             directorySelectionDialog.Description = "Please select the directory address of your repository:";
@@ -79,7 +89,79 @@ namespace FindChangedSignatureInGit
                     Console.WriteLine(ex.Message);
                     // instructionLbl.Text = ex.Message + "2222";
                 }
-               
+
+                #endregion
+
+
+                //find all modified functions' signatures
+                #region AllChangedSignatureFinder
+
+                try
+                {
+                    System.IO.File.WriteAllText(@"result.csv", ""); // create an empty file
+
+
+                    foreach (string line in File.ReadLines(@"commits.txt", Encoding.UTF8))
+                    {
+                        commitCounter++;
+                        System.IO.File.WriteAllText(@"eachCommitResult.txt", ""); // create an empty file                    
+
+                        //string mycommand = "git -C \"D:\\Git repo\\testGity\" diff " + line + "~ " + line + " >> eachCommitResult.txt";
+                        mycommand = "git -C \"" + repoAddress + "\"   diff " + line + "~ " + line + " >> eachCommitResult.txt";
+                        executeGitCommand(mycommand);
+
+                        System.Threading.Thread.Sleep(2000);
+
+                        foreach (string line2 in File.ReadLines(@"eachCommitResult.txt", Encoding.UTF8))
+                        {
+                            //Console.WriteLine("2");
+
+                            if (line2.StartsWith("+++")) //introduce the changed file
+                            {
+                                fileName = line2.Substring(6);
+                                fileCounter++;
+                                processStatusLbl.Text = commitCounter.ToString() + " Commit(s) and " + fileCounter.ToString() + " file(s) have been processed. ";
+                                Application.DoEvents();
+                            }
+                            if (line2.StartsWith("@@")) // introduce new hunk
+                            {
+                                if (fileName != "" && plusList.Count > 0)//we should write the result of previous hunk first and start the new hunk's processing
+                                {
+                                    reportPreviousHunkResults(plusList, minusList, line, fileName);
+                                }
+                                plusList.Clear();
+                                minusList.Clear();
+
+                            }//if @@
+
+                            if (line2.StartsWith("+") && !line2.StartsWith("+++") && javaFunctionsRegex.IsMatch(line2))
+                            {
+                                plusList.Add(line2);
+                            }
+                            if (line2.StartsWith("-") && !line2.StartsWith("---") && javaFunctionsRegex2.IsMatch(line2))
+                            {
+                                minusList.Add(line2);
+                            }
+
+                        }//eachcommit result
+
+                        if (fileName != "" && plusList.Count > 0)
+                        {
+                            reportPreviousHunkResults(plusList, minusList, line, fileName);
+                        }//if commit result is finished before checking the next commit
+                        plusList.Clear();
+                        minusList.Clear();
+
+                    }//each readed Commit line
+
+                }
+                catch (Exception ex)
+                {
+                   
+                    Console.WriteLine(ex.Message);
+                    instructionLbl.Text = ex.Message + "111";
+                }
+
                 #endregion
 
 
@@ -119,7 +201,55 @@ namespace FindChangedSignatureInGit
 
 
 
+        public void reportPreviousHunkResults(List<string> myPlusList, List<string> myMinusList, string MyLine, string myFileName)
+        {
+            try
+            {
+                string resultLine = "";
+                for (int i = 0; i < myPlusList.Count; i++)
+                {
+                    for (int j = 0; j < myMinusList.Count; j++)
+                    {
+                        int k = myPlusList[i].IndexOf('(');
+                        string plusFuncName = myPlusList[i].Substring(1, k - 1).Trim();
+                        k = plusFuncName.LastIndexOf(' ');
+                        plusFuncName = plusFuncName.Substring(k + 1); //name of function
+
+                        k = myMinusList[j].IndexOf('(');
+                        string minusFuncName = myMinusList[j].Substring(1, k - 1).Trim();
+                        k = minusFuncName.LastIndexOf(' ');
+                        minusFuncName = minusFuncName.Substring(k + 1); //name of function
+
+                        if (plusFuncName == minusFuncName)
+                        {
+                            string[] splitedplus = myPlusList[i].Split(',');
+                            string[] splitedminus = myMinusList[j].Split(',');
+                            if (splitedplus.Count() > splitedminus.Count())
+                            {
+                                resultGrid.Rows.Add();
+                                resultGrid[0, gridRowCounter].Value = MyLine;
+                                resultGrid[1, gridRowCounter].Value = myFileName;
+                                int k1 = myMinusList[j].IndexOf(')');
+                                resultGrid[2, gridRowCounter].Value = myMinusList[j].Substring(1, k1).Trim();
+                                int k2 = myPlusList[i].IndexOf(')');
+                                resultGrid[3, gridRowCounter].Value = myPlusList[i].Substring(1, k2).Trim();
+                                gridRowCounter++;
+                                resultLine = MyLine.Replace(',', ';') + "," + myFileName.Replace(',', ';') + "," + myMinusList[j].Substring(1, k1).Trim().Replace(',', ';') + "," + myPlusList[i].Substring(1, k2).Trim().Replace(',', ';');
+                                File.AppendAllText("result.csv", resultLine + "\n");
+                            }
+                        }
+                    }
+                }
 
 
-    }
+            }
+            catch (Exception ex)
+            {                
+                Console.WriteLine(ex.Message);
+                //instructionLbl.Text = ex.Message + "2222";
+            }
+        }
+
+
+        }
 }
